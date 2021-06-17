a217d1cb40602471a12ca3cd9979f02771e5ade8
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2021-06-17 00:23:15 -0700

    Add record to teams

--- blaseball/standings.py
+++ blaseball/standings.py
@@ -5,20 +5,27 @@ from blaseball_mike.models.season import Season
 
 sim = SimulationData.load()
 
+class TeamStanding:
+    def __init__ (self, team, standings):
+        self.team = team
+        self.wins = standings.wins[team.id]
+        self.losses = standings.losses[team.id]
+        self.games = standings.games_played[team.id]
+        self.unLosses = self.games - self.losses
+
+    def __repr__(self):
+        return f"[{self.wins}] {self.team.location} {self.team.nickname} ({self.unLosses}-{self.losses})"
 
 def get_top_teams(division):
     standings = Season.load(sim.season).standings
 
-    # Standings.wins is a dict {"TEAM-UUID": INT_NUM_WINS, ...}, sorted by wins
-    # We conve
-    # teams_from_standings_filtered = {t:w for (t,w) in season_standings.wins.items() if (t in division.teams)}
-
-    teams = list(division.teams.values())
-    teams.sort(key=lambda t: standings.wins[t.id], reverse=True)
+    # Standings.wins is a dict {"TEAM-UUID": INT_NUM_WINS, ...}
+    teams = [TeamStanding(team, standings) for team in division.teams.values()]
+    teams.sort(key=lambda t: t.wins, reverse=True)
     return teams
 
 
-# A list of 4 tuples of a division and a list of teams, sorted by Wins. Teams should have a .wins, .unLoses, and .loses
+# A list of 4 tuples of a division and a list of teams, sorted by Wins. Teams should have a .wins, .unLosses, and .losses
 def standings():
     divisions = chain.from_iterable([subleague.divisions.values() for subleague in sim.league.subleagues.values()])
 
@@ -31,8 +38,8 @@ def format_standings():
     s = ""
     for (d, teams) in standings():
         s += f"## {d.name}\n"
-        for t in teams:
-            s += f"{t.location} {t.nickname}\n"
+        for team in teams:
+            s += f"{team}\n"
         s += "\n"
     return s
 
